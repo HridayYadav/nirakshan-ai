5c9a3322a0265a2067bafb0412945b60cc092c00
HridayYadav <hridayrao2006@gmail.com>
2026-09-01 22:23:01 +0530

    Delete sign up directory

--- sign up/protected-route.js
+++
@@ -1,1 +0,0 @@
-(function () { const target = document.body.dataset.target || 'dashboard'; if (localStorage.getItem('nirikshanDemoSession') !== 'active') { location.replace('../login/'); return; } location.replace('../dashboard/#' + target); })();
